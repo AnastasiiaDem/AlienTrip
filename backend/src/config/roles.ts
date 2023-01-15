9fae44eae96585de2cc69d4081c60371127ad9c3
ChristinaGorokhovska <kristinagorokhovska@gmail.com>
2023-01-15 17:08:51 +0200

    api post

---
+++ backend/src/config/roles.ts
@@ -0,0 +1,7 @@
+const ROLES = {
+  User: 2001,
+  Moderator: 2002,
+  Admin: 2003,
+};
+
+export default ROLES;
---
+++ frontend/src/config/allowedRolesConfig.ts
@@ -0,0 +1,1 @@
+export const ALLOWED_ROLES = { User: 2001, Moderator: 2002, Admin: 2003 };
