29b78ecd38de81901b7de0bdf29523dd4f238f7d
AnastasiiaDem <85587393+AnastasiiaDem@users.noreply.github.com>
2023-01-16 02:45:18 +0200

    translation and style changes

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -2,6 +2,7 @@ import {Box, Button} from '@mui/material';
 import {Link} from 'react-router-dom';
 import Header from '../components/Header';
 import {useAuthContext} from '../context/AuthProvider';
+import Search from '../components/Search';
 
 export default function Home() {
   const {auth} = useAuthContext();
@@ -10,18 +11,18 @@ export default function Home() {
   return (
     <>
       <Header/>
-      <Box sx={{display: 'flex', flexDirection: 'column', alignItems: 'center'}}>
+      <Box>
         <Box marginTop={10}>
           <Link color="green" to={'/admin'}>Admin</Link>
           <Link to={'/moderator'}>Moderator</Link>
-          <Link to={'/info'}>Info</Link>
+          <Link to={'/info'}>Інформаційна сторінка</Link>
         </Box>
-        <Button sx={{marginTop: '10px'}} color="inherit" component={Link} to={'/create'}>
-          Create Post
+        <Button sx={{display: 'block', margin: '10px auto 30px auto', height: '30px', width: '170px'}} color="inherit" component={Link} to={'/create'} variant="contained">
+          Створити допис
         </Button>
       </Box>
-
-      <Search />
+      
+      <Search/>
     </>
   );
 }
